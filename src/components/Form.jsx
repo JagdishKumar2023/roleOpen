0300b8b938fc70edf67ca4c1207811896df6a56e
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2024-12-09 23:51:11 +0530

    role

---
+++ src/components/Form.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import "./Form.css";
+import axios from "axios";
+
+const Form = () => {
+  const [inputValues, setInputValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputValues({ ...inputValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputValues);
+    setInputValues({ name: "", email: "", password: "" }); // Reset all fields
+
+    axios
+      .post("http://localhost:4002/InputF", inputValues) // Replace with your API endpoint
+      .then((result) => {
+        console.log("Signup Success:", result);
+        alert("Signup successful!");
+      })
+      .catch((err) => {
+        console.log("Signup Error:", err);
+        alert("An error occurred. Please try again later.");
+      });
+  };
+
+  return (
+    <div className="form-container">
+      <form className="form" onSubmit={handleSubmit}>
+        <h2 className="form-heading">Interview Registration Form</h2>
+        <div className="form-group">
+          <label className="form-label" htmlFor="name">
+            User Name:
+          </label>
+          <input
+            className="form-input"
+            type="text"
+            value={inputValues.name}
+            name="name"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label className="form-label" htmlFor="email">
+            Email:
+          </label>
+          <input
+            className="form-input"
+            type="email"
+            value={inputValues.email}
+            name="email"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label className="form-label" htmlFor="password">
+            Password:
+          </label>
+          <input
+            className="form-input"
+            type="password"
+            value={inputValues.password}
+            name="password"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button className="form-button" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
